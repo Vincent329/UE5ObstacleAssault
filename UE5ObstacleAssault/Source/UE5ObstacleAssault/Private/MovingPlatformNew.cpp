d29254b8febb0e6c20f232553ce9e0a42e7c2a8a
Vincent329 <vincee.ho@gmail.com>
2024-10-17 17:15:04 -0400

    Moving Platform New

---
+++ UE5ObstacleAssault/Source/UE5ObstacleAssault/Private/MovingPlatformNew.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatformNew.h"
+
+// Sets default values
+AMovingPlatformNew::AMovingPlatformNew()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Scene Root"));
+	SetRootComponent(SceneRoot);
+
+	PlatformMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Platform Static Mesh"));
+	PlatformMesh->SetupAttachment(RootComponent);
+
+	EndPositionPoint = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("End Point"));
+	EndPositionPoint->SetupAttachment(RootComponent);
+	EndPositionPoint->bHiddenInGame = true;
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatformNew::BeginPlay()
+{
+	Super::BeginPlay();
+	StartPosition = GetActorLocation();
+	EndPosition = EndPositionPoint->GetComponentLocation();
+
+	f_Magnitude = FVector::Dist(StartPosition, EndPosition);
+	v_Velocity = (EndPosition - StartPosition).GetSafeNormal();
+}
+
+void AMovingPlatformNew::MovePlatform(float DeltaTime)
+{
+	FVector CurrentPosition = GetActorLocation();
+	CurrentPosition = CurrentPosition + (v_Velocity * f_Speed * DeltaTime);
+	SetActorLocation(CurrentPosition);
+
+	float f_distance = FVector::Dist(StartPosition, CurrentPosition);
+
+	if (f_distance > f_Magnitude)
+	{
+		FVector MoveDirection = v_Velocity.GetSafeNormal();
+		StartPosition = StartPosition + MoveDirection * f_distance;
+		SetActorLocation(StartPosition);
+		v_Velocity = -v_Velocity;
+	}
+}
+
+// Called every frame
+void AMovingPlatformNew::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	MovePlatform(DeltaTime);
+}
+
---
+++ UE5ObstacleAssault/Source/UE5ObstacleAssault/Public/MovingPlatformNew.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatformNew.generated.h"
+
+UCLASS()
+class UE5OBSTACLEASSAULT_API AMovingPlatformNew : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatformNew();
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Scene", meta =(DisplayName = "SceneRoot", ToolTip = "Scene Root"))
+	USceneComponent* SceneRoot;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh", meta =(DisplayName = "MeshComp", ToolTip = "The Mesh the Platform will Take"))
+	UStaticMeshComponent* PlatformMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh", meta = (DisplayName = "End Position", ToolTip = "The Mesh representing the distance"))
+	UStaticMeshComponent* EndPositionPoint;
+
+	UPROPERTY(EditAnywhere, meta = (Tooltip = "how fast this will go"))
+	float f_Speed = 10.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void MovePlatform(float DeltaTime);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	FVector StartPosition;
+
+	UPROPERTY(VisibleAnywhere)
+	FVector EndPosition;
+
+	UPROPERTY(VisibleAnywhere)
+	FVector v_Velocity;
+
+	UPROPERTY(VisibleAnywhere)
+	float f_Magnitude;
+	 
+};
